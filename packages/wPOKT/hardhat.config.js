a1689d0794ffe130150f1aab3fe47ff054f7bf39
Enrique Ortiz <hi@enriqueortiz.dev>
2021-11-29 14:25:04 +0000

    Hardhat: add deployment utils (#14)
    * feat: install hardhat-etherscan
    
    * chore: set up env vars
    
    * fix: only add pk if its in env

--- packages/wPOKT/hardhat.config.js
+++ packages/wPOKT/hardhat.config.js
@@ -1,10 +1,14 @@
 const { deploy } = require('./buidler/cli')
+require('dotenv').config()
 
+require('@nomiclabs/hardhat-etherscan')
 require('@nomiclabs/hardhat-truffle5')
 
 task('deploy', 'Deploy wPOKT')
   .addParam('minter', 'wPOKT minter')
   .setAction(deploy)
+
+const accounts = process.env.DEV_PK ? [process.env.DEV_PK ?? ''] : []
 
 module.exports = {
   networks: {
@@ -15,9 +19,11 @@ module.exports = {
     // Rinkeby network configured with Aragon node.
     rinkeby: {
       url: 'https://rinkeby.eth.aragon.network',
+      accounts,
     },
     kovan: {
       url: 'https://poa-kovan.gateway.pokt.network/v1/lb/61a4bf7a541b7a0039f10001',
+      accounts,
     },
     // Network configured to interact with Frame wallet. Requires
     // to have Frame running on your machine. Download it from:
@@ -25,7 +31,10 @@ module.exports = {
     frame: {
       httpHeaders: { origin: 'hardhat' },
       url: 'http://localhost:1248',
-    }
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
   },
   solidity: {
     version: '0.5.17',
